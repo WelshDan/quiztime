44ab2066e331f27d549c6462d2ca0a001826968e
WelshDan <dan.roberts78@gmail.com>
2023-04-05 12:49:21 +0000

    Create user request loop for target score

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ SHEET = GSPREAD_CLIENT.open('quiztime')
 
 questions = SHEET.worksheet('questions')
 
-def new_player():
+def player_name():
     """
     Get username from the user
     """
@@ -23,24 +23,34 @@ def new_player():
     username = input("Username: ")
     print(f"Welcome {username}!")
 
-new_player()
+player_name()
 
 def target_score():
     """
     Get user's target score out of 10
     """
-    print("The quiz contains 10 questions.\nWhat is your target score out of 10?")
-    target_score = input("My goal is: ")
-    target = range(1,10)
-    try:
-        if target_score is not target:
-            raise ValueError(
-                f"Your target must be between 1 and 10, you provided {target_score}"
-            )
-    except ValueError as error:
-        print(f"Invalid data: {error}, please try again.\n")
+    while True:
+        print("The quiz contains 10 questions.\nWhat is your target score out of 10?")
+        target_score = input("My goal is: ")
+        target = range(1,10)
+        print(target_score)
+        try:
+            if target_score is target:
+                raise ValueError(
+                    f"Your target must be between 1 and 10, you provided {target_score}"
+                    )
+        except ValueError as error:
+            print(f"Invalid data: {error}, please try again.\n")
+            return False
+        else:
+            print("Can you beat that target? Good luck with the quiz.")
+            return True
 
 target_score()
+
+#def question_test()
+
+
 
 #data = questions.get_all_values()
 
