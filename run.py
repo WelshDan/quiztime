5f239a8dd5d4b2d1a0fe76514e1aaab8726333dc
WelshDan <dan.roberts78@gmail.com>
2023-04-06 14:00:22 +0000

    Create questions loop and input score function

--- run.py
+++ run.py
@@ -15,6 +15,10 @@ SHEET = GSPREAD_CLIENT.open('quiztime')
 
 questions = SHEET.worksheet('Alist')
 
+TOTAL_QUESTIONS = 10
+score = 0
+questions_list = []
+question_index = 0
 
 def player_name():
     """
@@ -25,7 +29,6 @@ def player_name():
     username = input("Username: ")
     print(f"Welcome {username}!")
 
-player_name()
 
 def target_score():
     """
@@ -35,7 +38,7 @@ def target_score():
     \n repeat until a figure between 1 and 10 is entered.
     """
     while True:
-        print("The quiz contains 10 questions.\nWhat is your target score out of 10?")
+        print("The quiz contains " + str(TOTAL_QUESTIONS) + "questions.\nWhat is your target score out of 10?")
         target_score = input("My goal is: ")
         target = range(1, 10)
         print(f"OK, good luck trying to get more than {target_score}!")
@@ -51,43 +54,37 @@ def target_score():
             print("Can you beat that target? Good luck with the quiz.")
             return True
 
-target_score()
-
 def ask_question():
     """
     Locate the first question in the list
     """
-    question_number = questions.acell("A2").value
-    print(f" Here is question {question_number}...")
-    question = questions.acell("B2").value
-    print(question)
-
-ask_question()
+    question = questions_list[question_index + 1]
+    print(f" Here is question {question[0]}...")
+    print(question[1])
 
 def save_answer():
     """
     User enters answer with a letter between A and D.
     Answer is logged for later results.
     """
+    global score
     while True:
         print("What is your answer? A,B,C or D")
-        answer = input("My answer is ")
+        answer = input("My answer is ").lower()
         print(f"You've guessed answer {answer}")
-        possible_answers = ("a", "A", "B", "b", "C", "c", "D", "d")
+        possible_answers = ["a", "b", "c", "d"]
         try:
-            answer == possible_answers
-                if answer != possible_answers
-                    raise KeyError(
-                    f"You can only answer with A, B, C or D"
-                    )
+            if answer not in possible_answers:
+                raise ValueError(f"You can only answer with A, B, C or D")
+            else: 
+                if questions_list[question_index + 1][2].lower() == answer:
+                    score = score + 1
+                print("Thank you for your answer.")
+                break
         except ValueError as error:
                 print(f"Invalid data: {error}, please try again.\n")
-        return False
-        else:
-            print("Thank you for your answer.")
-            return True
+       
 
-save_answer()
 
 #ask_next_question()
 
@@ -98,3 +95,23 @@ save_answer()
 #close_quiz
 
 #print(data)
+
+def initialise_questions():
+    global questions_list
+    questions_list = questions.get_values()
+
+
+def main():
+    global question_index
+    initialise_questions()
+    player_name()
+    target_score()
+    while question_index < TOTAL_QUESTIONS:
+        ask_question()
+        save_answer()
+        question_index = question_index + 1
+    print(f"\n\n\n\n\n\nYour score is: {score}\n")
+
+
+
+main()
