e89f18739a6b1b4f0cb6f81fcdd3a6ca4e0c4f8d
WelshDan <dan.roberts78@gmail.com>
2023-04-08 22:31:16 +0000

    Fixed errors and started display results function

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,9 +15,10 @@ SHEET = GSPREAD_CLIENT.open('quiztime')
 questions = SHEET.worksheet('Alist')
 
 TOTAL_QUESTIONS = 10
-score = 0
-questions_list = []
-question_index = 0
+SCORE = 0
+QUESTIONS_LIST = []
+QUESTIONS_INDEX = 0
+
 
 def player_name():
     """
@@ -38,14 +38,16 @@ def target_score():
     \n repeat until a figure between 1 and 10 is entered.
     """
     while True:
-        print("The quiz contains " + str(TOTAL_QUESTIONS) + "questions.\nWhat is your target score out of 10?")
+        print("The quiz contains " + str(TOTAL_QUESTIONS) + "questions.\n\
+            What is your target score out of 10?")
         target_score = input("My goal is: ")
         target = range(1, 10)
         print(f"OK, good luck trying to get more than {target_score}!")
         try:
             if target_score is target:
                 raise ValueError(
-                    f"Your target must be between 1 and 10, you provided {target_score}"
+                    f"Your target must be between 1 and 10, you provided\
+                         {target_score}"
                     )
         except ValueError as error:
             print(f"Invalid data: {error}, please try again.\n")
@@ -54,13 +56,16 @@ def target_score():
             print("Can you beat that target? Good luck with the quiz.")
             return True
 
+
 def ask_question():
     """
-    Locate the first question in the list
+    Locate the first question in the list. Repeat \
+        until all 10 questions are asked.
     """
     question = questions_list[question_index + 1]
     print(f" Here is question {question[0]}...")
     print(question[1])
+
 
 def save_answer():
     """
@@ -75,26 +80,38 @@ def save_answer():
         possible_answers = ["a", "b", "c", "d"]
         try:
             if answer not in possible_answers:
-                raise ValueError(f"You can only answer with A, B, C or D")
-            else: 
+                raise ValueError("You can only answer with A, B, C or D")
+            else:
                 if questions_list[question_index + 1][2].lower() == answer:
                     score = score + 1
                 print("Thank you for your answer.")
                 break
         except ValueError as error:
-                print(f"Invalid data: {error}, please try again.\n")
-       
+            print(f"Invalid data: {error}, please try again.\n")
 
 
-#ask_next_question()
+def display_results():
+    """
+    Target score and actual score are compared and the results are returned.
+    """
+    final_score = score if questions == 10
 
-#check_quiz_complete()
+    if final_score > target_score:
+        print(f"Sadly, your target score was {target_score} but you only got {final_score}.")
+        print("You are not as clever as you think")
+    if final_score == target_score:
+        print(f"Well done! Your target score was {target_score} and you matched that it!")
+        print(f" You scored {final_score}. You know exactly how clever you are!")
+    if final_score < target_score:
+        print(f"Congratulations! Your target was {target_score} but you scored {final_score}")
+        print(f"You are much smarter than you think you are!")
 
-#display_results
+display_results()
 
 #close_quiz
 
 #print(data)
+
 
 def initialise_questions():
     global questions_list
@@ -111,7 +128,6 @@ def main():
         save_answer()
         question_index = question_index + 1
     print(f"\n\n\n\n\n\nYour score is: {score}\n")
-
 
 
 main()
