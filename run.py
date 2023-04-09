b8a32db67e107b5b0277291345c35655856a4bb5
WelshDan <dan.roberts78@gmail.com>
2023-04-09 15:35:26 +0000

    Add welcome screen and delays to create better flow

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import time
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,7 +18,19 @@ questions = SHEET.worksheet('Alist')
 TOTAL_QUESTIONS = 10
 SCORE = 0
 QUESTIONS_LIST = []
-QUESTIONS_INDEX = 0
+QUESTION_INDEX = 0
+
+
+def welcome_screen():
+    """
+    Start screen that welcomes the user
+    """
+    print("Hello")
+    time.sleep(3)
+    print("Welcome to...")
+    time.sleep(3)
+    print("QUIZTIME")
+    time.sleep(3)
 
 
 def player_name():
@@ -26,8 +39,10 @@ def player_name():
     """
     print("Please enter your name")
 
-    username = input("Username: ")
+    username = input("My name is ")
+    time.sleep(3)
     print(f"Welcome {username}!")
+    time.sleep(3)
 
 
 def target_score():
@@ -38,22 +53,23 @@ def target_score():
     \n repeat until a figure between 1 and 10 is entered.
     """
     while True:
-        print("The quiz contains " + str(TOTAL_QUESTIONS) + "questions.\n\
-            What is your target score out of 10?")
+        print("The quiz contains " + str(TOTAL_QUESTIONS) + " questions.\
+            What is your target score?\n")
         target_score = input("My goal is: ")
         target = range(1, 10)
         print(f"OK, good luck trying to get more than {target_score}!")
         try:
             if target_score is target:
                 raise ValueError(
-                    f"Your target must be between 1 and 10, you provided\
-                         {target_score}"
+                    f"Your target must be between 1 and 10, \
+                        you provided {target_score}"
                     )
         except ValueError as error:
             print(f"Invalid data: {error}, please try again.\n")
             return False
         else:
-            print("Can you beat that target? Good luck with the quiz.")
+            time.sleep(3)
+            print("Can you beat that target?")
             return True
 
 
@@ -63,7 +79,7 @@ def ask_question():
         until all 10 questions are asked.
     """
     question = questions_list[question_index + 1]
-    print(f" Here is question {question[0]}...")
+    print(f" Here is question {question[0]}...\n")
     print(question[1])
 
 
@@ -74,7 +90,7 @@ def save_answer():
     """
     global score
     while True:
-        print("What is your answer? A,B,C or D")
+        print("What is your answer? A,B,C or D\n")
         answer = input("My answer is ").lower()
         print(f"You've guessed answer {answer}")
         possible_answers = ["a", "b", "c", "d"]
@@ -85,32 +101,10 @@ def save_answer():
                 if questions_list[question_index + 1][2].lower() == answer:
                     score = score + 1
                 print("Thank you for your answer.")
+                time.sleep(3)
                 break
         except ValueError as error:
             print(f"Invalid data: {error}, please try again.\n")
-
-
-def display_results():
-    """
-    Target score and actual score are compared and the results are returned.
-    """
-    final_score = score if questions == 10
-
-    if final_score > target_score:
-        print(f"Sadly, your target score was {target_score} but you only got {final_score}.")
-        print("You are not as clever as you think")
-    if final_score == target_score:
-        print(f"Well done! Your target score was {target_score} and you matched that it!")
-        print(f" You scored {final_score}. You know exactly how clever you are!")
-    if final_score < target_score:
-        print(f"Congratulations! Your target was {target_score} but you scored {final_score}")
-        print(f"You are much smarter than you think you are!")
-
-display_results()
-
-#close_quiz
-
-#print(data)
 
 
 def initialise_questions():
@@ -118,16 +112,18 @@ def initialise_questions():
     questions_list = questions.get_values()
 
 
+question_index = question_index + 1
+
 def main():
     global question_index
     initialise_questions()
+    welcome_screen()
     player_name()
     target_score()
     while question_index < TOTAL_QUESTIONS:
         ask_question()
         save_answer()
-        question_index = question_index + 1
-    print(f"\n\n\n\n\n\nYour score is: {score}\n")
+    print("Thank you for taking part in the quiz")
 
 
 main()
