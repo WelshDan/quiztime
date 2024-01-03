1575a99711f090376a9fb7c6acc2ef8e02a504e9
WelshDan <>
2024-01-03 23:57:40 +0000

    changed time sleep length

--- run.py
+++ run.py
@@ -37,11 +37,11 @@ def show_welcome_screencreen():
     Start screen that welcomes the user
     """
     print("Hello!\n")
-    # time.sleep(2)
+    # time.sleep(0.5)
     print("and welcome to...\n")
-    # time.sleep(3)
+    # time.sleep(1)
     print("QUIZTIME\n")
-    # time.sleep(2)
+    # time.sleep(0.5)
 
 
 def get_player_name():
@@ -55,9 +55,9 @@ def get_player_name():
             break
         print("Name cannot be empty. Please enter your name")
 
-    # time.sleep(1)
+    # time.sleep(0.5)
     print(f"Welcome {username}!")
-    # time.sleep(1)
+    # time.sleep(0.5)
     return username
 
 
@@ -68,13 +68,13 @@ def choose_difficulty():
     with containing a set of easy, medium or hard questions
     """
     print("You will now need to choose the difficulty level of the quiz \n")
-    # time.sleep(1)
+    # time.sleep(0.5)
     print("You can select from easy, medium or hard\n")
-    # time.sleep(1)
+    # time.sleep(0.5)
     print("Select your difficulty by pressing 1,2 or 3 and then enter\n")
-    # time.sleep(1)
+    # time.sleep(0.5)
     print("1 = Easy , 2 = Medium, 3 = Hard\n")
-    # time.sleep(1)
+    # time.sleep(0.5)
 
     while True:
         try:
@@ -104,10 +104,10 @@ def target_score():
     while True:
         print("The quiz contains " + str(TOTAL_QUESTIONS) + " questions.\n")
         print("What is your target score?\n")
-        # time.sleep(2)
+        # time.sleep(0.5)
         try:
             users_goal = int(input("My goal is: \n"))
-            # time.sleep(1)
+            # time.sleep(0.5)
             if users_goal not in range(1, 11):
                 raise ValueError(f"Your target must be between 1 and 10. You provided {users_goal}\n")
             else:
@@ -129,7 +129,7 @@ def ask_question():
     """
     question = questions_list[question_index + 1]
     print(f"Here is question {question[0]}...\n")
-    # time.sleep(2)
+    # time.sleep(0.5)
     print(question[1])
 
 
@@ -141,9 +141,9 @@ def accept_answer():
     global score
     while True:
         print("What is your answer? (A,B,C or D)\n")
-        # time.sleep(2)
+        # time.sleep(0.5)
         answer = input("My answer is \n").lower()
-        # time.sleep(2)
+        # time.sleep(0.5)
         correct_answer = questions_list[question_index + 1][2].lower()
         possible_answers = ["a", "b", "c", "d"]
         try:
@@ -157,7 +157,7 @@ def accept_answer():
                     print(f"Thank you for your answer.\nThe correct answer was {correct_answer}\n")
                     print(f"The answer {answer} was incorrect\n")
                 print(f"Your current score is {score} \n")
-                # time.sleep(2)
+                # time.sleep(0.5)
                 break
         except ValueError as error:
             print(f"Invalid data: {error}, please try again.\n")
@@ -168,28 +168,30 @@ def display_results(username, users_goal):
     Target score and actual score are compared and the results are returned
     """
     print(f"Your final score is {score}\n")
-    # time.sleep(2)
+    # time.sleep(0.5)
 
     if score > users_goal:
         print(f"Sadly, your target score was {users_goal} but you only got {score}\n")
-        # time.sleep(1)
+        # time.sleep(0.5)
         print("You just missed your target, bad luck!\n")
     elif score == users_goal:
         print(f"Well done! Your target score was {users_goal} and you matched that it!\n")
-        # time.sleep(1)
+        # time.sleep(0.5)
         print("You hit your goal, well done!\n")
     else:
         print(f"Congratulations! Your target was {users_goal} but you scored {score}!")
-        # time.sleep(1)
+        # time.sleep(0.5)
         print("You beat your goal! Excellent work!\n")
 
     print("Scores are being saved to the history books\n")
+    # time.sleep(0.5)
     data = [username, score]
     saves_worksheet = SHEET.worksheet("results")
     saves_worksheet.append_row(data)
 
     print("Thank you for playing \n\n ")
-    print("(Dan Roberts 2023)")
+    # time.sleep(0.5)
+    print("(Created by Dan Roberts 2023)")
 
 def start_quiz(username, users_goal):
     global question_index
